02fa234185d57ff539fc28e2f853c774c96e2b5b
Sergey Ivolga <zoinx2010@mail.ru>
2017-06-21 01:05:34 +0400

    2 одинаковых поля

--- web.php
+++ web.php
@@ -26,4 +26,3 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('specializations','SpecializationController');
 Route::resource('specializations','SpecializationController');
-Route::resource('specializations','SpecializationController');
